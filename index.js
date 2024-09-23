582be5537cdf3fab51d8f333a8649b94eec1433f
melrwa <orwamelkizedek@gmail.com>
2024-09-23 14:07:02 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code in this file!
+const currentUser='Melkizedek!';
+const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting=`Welcome, ${currentUser[0]}!`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
